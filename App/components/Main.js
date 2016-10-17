e0edfa55dcc4d28088ccbc4e2d32a88fd44dadb8
TJAlston <tjalston82@gmail.com>
2016-10-16 21:43:10 -0400

    image added

--- App/components/Main.js
+++ App/components/Main.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import {
   StyleSheet,
+  Image,
   Text,
   View
 } from 'react-native'
@@ -8,12 +9,25 @@ import {
 const styles = StyleSheet.create({
   instructions: {
     flex: 1,
+    flexDirection: 'column',
     justifyContent: 'center',
-    backgroundColor: 'rgb(201, 237, 240)'
+    alignItems: 'center',
+    margin: 5
   },
-  word: {
-    color: '#1a7fc8',
+  title: {
+    color: '#0a5993',
     fontSize: 20
+  },
+  paragraph: {
+    color: '#1a1f22',
+    fontSize: 12,
+    textAlign: 'center',
+    marginTop: 5
+  },
+  headshot: {
+    height: 300,
+    width: 200,
+    marginBottom: 5
   }
 })
 
@@ -21,7 +35,9 @@ class Main extends Component {
   render () {
     return (
       <View style= {styles.instructions}>
-        <Text style ={styles.word}> Testing the Router</Text>
+      <Image style={styles.headshot} source={require('../images/Headshot2.jpg')} />
+        <Text style ={styles.title}> Web Designer</Text>
+        <Text style={styles.paragraph}>Thank you for taking the time to view my page. I am a Web Designer and Developer that has experience working with HTML, CSS and Javascript. I am also a recent graduate of The Iron Yard with a certification in Front-End Engineering.</Text>
       </View>
     )
   }
